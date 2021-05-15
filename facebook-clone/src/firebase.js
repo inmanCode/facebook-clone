96ff041a7474fd63401f074ed4c364bdce924814
aymaneabssi <78247123+aymaneabssi@users.noreply.github.com>
2021-05-15 21:34:48 +0200

    Header component

---
+++ facebook-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBPsJo3pZjv2lgkeubuJ-lbT8QUMuv5xC0',
+  authDomain: 'facebook-clone-744d5.firebaseapp.com',
+  projectId: 'facebook-clone-744d5',
+  storageBucket: 'facebook-clone-744d5.appspot.com',
+  messagingSenderId: '35121854500',
+  appId: '1:35121854500:web:91cfe04f117de680f100b1',
+};
